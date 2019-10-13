2f691b5686627bb1cc104e37d62a02ade2c4e2d3
Answer <mazurwro@gmail.com>
2019-10-13 20:22:54 +0200

    update v 0.4 wprowadzono zapis konstruktorów z metodą this.

--- src/Book.java
+++ src/Book.java
@@ -8,13 +8,17 @@
     String isbn;
 
      Book(String title, String author, int releaseDate, int pages, String publisher, String isbn) {
-        this.title = title;
-        this.author = author;
-        this.releaseDate = releaseDate;
-        this.pages = pages;
-        this.publisher = publisher;
+        this(title,author, releaseDate, pages, publisher);
         this.isbn = isbn;
     }
+     Book(String title, String author, int releaseDate, int pages, String publisher) {
+         this.title = title;
+         this.author = author;
+         this.releaseDate = releaseDate;
+         this.pages = pages;
+         this.publisher = publisher;
+
+     }
 
 
     void printInfo() {
--- src/Library.java
+++ src/Library.java
@@ -1,12 +1,12 @@
  class Library {
     public static void main(String[] args) {
-        final String appName = "Bilbioteka v 0.3";
+        final String appName = "Bilbioteka v 0.4";
         Book book1 = new Book("W pustyni i w pusczy", "Henryk Sienkiewicz",
                 2010, 296, "Greg", "978837271890" );
         Book book2 = new Book("Java. Efektywne programowanie. WYdanie II", "Joshua Bloch",
                 2009, 352, "Helion", "9788324620845" );
         Book book3 = new Book("SCJP Sun Certified Programmer for Java 6 Study Guide", "Bert Bates, Katherine Sierra",
-                2008, 851, "McGraw-Hill Osborne Media", "9780071591065" );
+                2008, 851, "McGraw-Hill Osborne Media");
 
         System.out.println(appName);
         System.out.println("Książki dostęne w bibliotece:");
