9f601b0f825f97cc5e978dc071185bf3429551d1
Answer <mazurwro@gmail.com>
2019-10-13 21:33:58 +0200

    update v 0.5 wprowadzony standard zgodnosci z JavaBeans
    gettery i settery

--- src/pl/javastart/library/app/Library.java
+++ src/pl/javastart/library/app/Library.java
@@ -1,6 +1,10 @@
- class Library {
+package pl.javastart.library.app;
+
+import pl.javastart.library.model.Book;
+
+public class Library {
     public static void main(String[] args) {
-        final String appName = "Bilbioteka v 0.4";
+        final String appName = "Bilbioteka v 0.5";
         Book book1 = new Book("W pustyni i w pusczy", "Henryk Sienkiewicz",
                 2010, 296, "Greg", "978837271890" );
         Book book2 = new Book("Java. Efektywne programowanie. WYdanie II", "Joshua Bloch",
